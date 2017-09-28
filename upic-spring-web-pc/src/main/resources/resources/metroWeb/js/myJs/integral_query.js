d23c1b3568c3c2b980bcefa7ec204cee215dbec3
zhubuqing <1055793362@qq.com>
2017-09-28 13:03:53 +0800

    20170927更新

--- upic-spring-web-pc/src/main/resources/resources/metroWeb/js/myJs/integral_query.js
+++ upic-spring-web-pc/src/main/resources/resources/metroWeb/js/myJs/integral_query.js
@@ -46,7 +46,7 @@ function ajaxs(datas, method, urls) {
 function addHtmls(result, method) {
     var htmls = "";
     var htmlss = "";
-    if (method == "showAll") {
+    if (method === "showAll") {
         for (var i = 0; i < result.length; i++) {
             var color = "";
             var statusC = "";
@@ -79,45 +79,44 @@ function addHtmls(result, method) {
             htmls += "<td>";
             htmls += "<a href='#mymodal1' data-toggle='modal'><div class='message_div'>查看详情</div></a></td>";
             htmls += "</tr>";
-
-            htmlss += "<div class='row-form clearfix'>";
-            htmlss += "<div class='span3'>编号</div>";
-            htmlss += "<div class='span3'>" + (i + 1) + "</div>";
-            htmlss += "<div class='span3'>代码</div>";
-            htmlss += "<div class='span3'>" + result[i].integralLogId.projectNum + "</div>";
-            htmlss += "</div>";
-            htmlss += "<div class='row-form clearfix'>";
-            htmlss += "<div class='span3'>项目申请日期</div>";
-            htmlss += "<div class='span3'>" + result[i].creatTime + "</div>";
-            htmlss += "<div class='span3'>状态</div>";
-            htmlss += "<div class='span3'>" + statusC + "</div>";
-            htmlss += "</div>";
-            htmlss += "<div class='row-form clearfix'>";
-            htmlss += "<div class='span3'>项目类别</div>";
-            htmlss += "<div class='span3'>" + splitJson(result[i].event) + "</div>";
-            htmlss += "<div class='span3'>所属学院</div>";
-            htmlss += "<div class='span3'>" + result[i].college + "</div>";
-            htmlss += "</div>";
-            htmlss += "<div class='row-form clearfix'>";
-            htmlss += "<div class='span3'>项目名称</div>";
-            htmlss += "<div class='span3'>" + result[i].projectName + "</div>";
-            htmlss += "<div class='span3'>负责人</div>";
-            htmlss += "<div class='span3'>XXX</div>";
-            htmlss += "</div>";
-            htmlss += "<div class='row-form clearfix'>";
-            htmlss += "<div class='span3'>项目内容</div>";
-            htmlss += "<div class='span9'>校级学生干部加分校级学生干部加分校级学生干部加分校级学生干部加分校级学生干部加分校级学生干部加分</div>";
-            htmlss += "</div>";
-            htmlss += "<div class='row-form clearfix'>";
-            htmlss += "<div class='span3'>评价标准与形式</div>";
-            htmlss += "<div class='span9'>校级学生干部加分校级学生干部加分校级学生干部加分校级学生干部加分校级学生干部加分校级学生干部加分</div>";
-            htmlss += "</div>";
         }
-
-        $("#showAll").html(htmls);
-        $("#details").html(htmlss);
-
+    } else if (method === "details") {
+        htmlss += "<div class='row-form clearfix'>";
+        htmlss += "<div class='span3'>编号</div>";
+        htmlss += "<div class='span3'>" + (i + 1) + "</div>";
+        htmlss += "<div class='span3'>代码</div>";
+        htmlss += "<div class='span3'>" + result[i].integralLogId.projectNum + "</div>";
+        htmlss += "</div>";
+        htmlss += "<div class='row-form clearfix'>";
+        htmlss += "<div class='span3'>项目申请日期</div>";
+        htmlss += "<div class='span3'>" + result[i].creatTime + "</div>";
+        htmlss += "<div class='span3'>状态</div>";
+        htmlss += "<div class='span3'>" + statusC + "</div>";
+        htmlss += "</div>";
+        htmlss += "<div class='row-form clearfix'>";
+        htmlss += "<div class='span3'>项目类别</div>";
+        htmlss += "<div class='span3'>" + splitJson(result[i].event) + "</div>";
+        htmlss += "<div class='span3'>所属学院</div>";
+        htmlss += "<div class='span3'>" + result[i].college + "</div>";
+        htmlss += "</div>";
+        htmlss += "<div class='row-form clearfix'>";
+        htmlss += "<div class='span3'>项目名称</div>";
+        htmlss += "<div class='span3'>" + result[i].projectName + "</div>";
+        htmlss += "<div class='span3'>负责人</div>";
+        htmlss += "<div class='span3'>XXX</div>";
+        htmlss += "</div>";
+        htmlss += "<div class='row-form clearfix'>";
+        htmlss += "<div class='span3'>项目内容</div>";
+        htmlss += "<div class='span9'>校级学生干部加分校级学生干部加分校级学生干部加分校级学生干部加分校级学生干部加分校级学生干部加分</div>";
+        htmlss += "</div>";
+        htmlss += "<div class='row-form clearfix'>";
+        htmlss += "<div class='span3'>评价标准与形式</div>";
+        htmlss += "<div class='span9'>校级学生干部加分校级学生干部加分校级学生干部加分校级学生干部加分校级学生干部加分校级学生干部加分</div>";
+        htmlss += "</div>";
     }
+
+    $("#showAll").html(htmls);
+    $("#details").html(htmlss);
 }
 
 function splitJson(json) {
@@ -125,3 +124,115 @@ function splitJson(json) {
     projectCategorys.concat(json.split(" >> "));
     return projectCategorys[0];
 }
+
+//*************************************************************************************************************************************
+
+var Script = function () {
+    // begin first table
+    $('#sample_1,#sample_1_1,#sample_1_2,#sample_1_3').dataTable({
+        "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+        "sPaginationType": "bootstrap",
+        "oLanguage": {
+            "sLengthMenu": "每页显示 _MENU_ 条记录",
+            "sZeroRecords": "对不起，查询不到任何相关数据",
+            "sInfo": "当前显示 _START_ 到 _END_ 条，共 _TOTAL_条记录",
+            "sInfoEmtpy": "找不到相关数据",
+            "sInfoFiltered": "数据表中共为 _MAX_ 条记录)",
+            "sProcessing": "正在加载中...",
+            "sSearch": " ",
+            "oPaginate": {
+                "sFirst": "第一页",
+                "sPrevious": " 上一页 ",
+                "sNext": " 下一页 ",
+                "sLast": " 最后一页 "
+            },
+        },
+        "aLengthMenu": [5, 10, 25, 50, 100],
+        "aoColumnDefs": [{
+            'bSortable': false,
+            'aTargets': [0]
+        }]
+    });
+
+    jQuery('#sample_1,#sample_1_1,#sample_1_2,#sample_1_3 .group-checkable').change(function () {
+        var set = jQuery(this).attr("data-set");
+        var checked = jQuery(this).is(":checked");
+        jQuery(set).each(function () {
+            if (checked) {
+                $(this).attr("checked", true);
+            } else {
+                $(this).attr("checked", false);
+            }
+        });
+        jQuery.uniform.update(set);
+    });
+
+    jQuery('#sample_1_wrapper .dataTables_filter input').addClass("input-medium"); // modify table search input
+    jQuery('#sample_1_wrapper .dataTables_length select').addClass("input-mini"); // modify table per page dropdown
+
+    // begin second table
+    $('#sample_2').dataTable({
+        "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+        "sPaginationType": "bootstrap",
+        "oLanguage": {
+            "sLengthMenu": "_MENU_ per page",
+            "oPaginate": {
+                "sPrevious": "Prev",
+                "sNext": "Next"
+            }
+        },
+        "aoColumnDefs": [{
+            'bSortable': false,
+            'aTargets': [0]
+        }]
+    });
+
+    jQuery('#sample_2 .group-checkable').change(function () {
+        var set = jQuery(this).attr("data-set");
+        var checked = jQuery(this).is(":checked");
+        jQuery(set).each(function () {
+            if (checked) {
+                $(this).attr("checked", true);
+            } else {
+                $(this).attr("checked", false);
+            }
+        });
+        jQuery.uniform.update(set);
+    });
+
+    jQuery('#sample_2_wrapper .dataTables_filter input').addClass("input-small"); // modify table search input
+    jQuery('#sample_2_wrapper .dataTables_length select').addClass("input-mini"); // modify table per page dropdown
+
+    // begin: third table
+    $('#sample_3').dataTable({
+        "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+        "sPaginationType": "bootstrap",
+        "oLanguage": {
+            "sLengthMenu": "_MENU_ per page",
+            "oPaginate": {
+                "sPrevious": "Prev",
+                "sNext": "Next"
+            }
+        },
+        "aoColumnDefs": [{
+            'bSortable': false,
+            'aTargets': [0]
+        }]
+    });
+
+    jQuery('#sample_3 .group-checkable').change(function () {
+        var set = jQuery(this).attr("data-set");
+        var checked = jQuery(this).is(":checked");
+        jQuery(set).each(function () {
+            if (checked) {
+                $(this).attr("checked", true);
+            } else {
+                $(this).attr("checked", false);
+            }
+        });
+        jQuery.uniform.update(set);
+    });
+
+    jQuery('#sample_3_wrapper .dataTables_filter input').addClass("input-small"); // modify table search input
+    jQuery('#sample_3_wrapper .dataTables_length select').addClass("input-mini"); // modify table per page dropdown
+}();
