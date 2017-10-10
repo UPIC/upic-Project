51bdee6d638553279bba6ad2c0ce46f51668dc96
upic <2487718845@qq.com>
2017-10-10 17:08:07 +0800

    覆盖重写

--- upic-spring-grainCoin-provider/src/main/java/com/upic/serviceimpl/IntegralLogServiceImpl.java
+++ upic-spring-grainCoin-provider/src/main/java/com/upic/serviceimpl/IntegralLogServiceImpl.java
@@ -1,6 +1,8 @@
 package com.upic.serviceimpl;
 
 import com.upic.common.beans.utils.UpicBeanUtils;
+import com.upic.common.exception.NeedRollBackException;
+import com.upic.common.exception.NoExitException;
 import com.upic.common.support.spec.domain.AbstractDomain2InfoConverter;
 import com.upic.common.support.spec.domain.converter.QueryResultConverter;
 import com.upic.common.utils.redis.UpicRedisComponent;
@@ -20,7 +22,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.test.util.AopTestUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,10 +29,11 @@ import java.util.Queue;
 
 @Service("integralLogService")
 public class IntegralLogServiceImpl implements IntegralLogService {
-
     @Autowired
     private IntegralLogRepository integralLogRepository;
 
+    @Autowired
+    private UpicRedisComponent redisComponent;
     protected static final Logger LOGGER = LoggerFactory.getLogger(IntegralLogServiceImpl.class);
 
     public void allOperation(String apartment, String operatorNum, IntegralLogStatusEnum status, IntegralLogTypeEnum type) {
@@ -111,8 +113,29 @@ public class IntegralLogServiceImpl implements IntegralLogService {
         }
     }
 
-    public void signUp(IntegralLogInfo integralLogInfo) {
-
+    @Override
+    public IntegralLogInfo signUp(IntegralLogInfo integralLogInfo, int allNum) {
+    	String projectKey=integralLogInfo.getIntegralLogId().getProjectNum();
+    	try {
+    		Long increment = redisComponent.increment(projectKey);
+    		if(increment == -1L) {
+    			throw new NoExitException("No project!");
+    		}
+    		if(!redisComponent.putIfAbsent(projectKey+"hash", integralLogInfo.getIntegralLogId().getStudentNum(), increment+"")) {
+    			throw new NeedRollBackException("exit!");
+    		}
+    		integralLogInfo.setField1(increment+"");
+    		IntegralLog i=new IntegralLog();
+    		UpicBeanUtils.copyProperties(integralLogInfo, i);
+    		i=integralLogRepository.save(i);
+    		UpicBeanUtils.copyProperties(i, integralLogInfo);
+    		return integralLogInfo;
+    	}catch (Exception e) {
+    		if(e instanceof NeedRollBackException) {
+    			redisComponent.decrement(projectKey);
+    		}
+    		return null;
+		}
     }
 
     public Page<IntegralLogInfo> searchIntegralLog(IntegralLogCondition integralLogCondition, Pageable pageable) {
@@ -199,7 +222,7 @@ public class IntegralLogServiceImpl implements IntegralLogService {
         }
     }
 
-    public Page<IntegralLogInfo> integralLogSearchBar(String status, String keyword, Pageable pageable) {
+    public Page<IntegralLogInfo> integralLogSearchBar(IntegralLogStatusEnum status, String keyword, Pageable pageable) {
         Page<IntegralLog> integralLogPage = null;
         try {
             integralLogPage = integralLogRepository.integralLogSearchBar(status, keyword, pageable);
@@ -250,236 +273,5 @@ public class IntegralLogServiceImpl implements IntegralLogService {
         }
     }
 
-	@Autowired
-	private IntegralLogRepository integralLogRepository;
-
-	protected static final Logger LOGGER = LoggerFactory.getLogger(IntegralLogServiceImpl.class);
-
-	@Autowired
-	private UpicRedisComponent redisComponent;
-
-	public void allOperation(String apartment, String operatorNum, IntegralLogStatusEnum status,
-			IntegralLogTypeEnum type) {
-		try {
-			List<IntegralLog> integralLogs = integralLogRepository.getByApartmentAndType(apartment, type);
-			for (IntegralLog integralLog : integralLogs) {
-				integralLog.setStatus(status);
-				integralLogRepository.saveAndFlush(integralLog);
-			}
-		} catch (Exception e) {
-			LOGGER.info("allOperation:全部操作失败。错误信息：" + e.getMessage());
-			e.printStackTrace();
-		}
-	}
-
-	public Queue<Long> someOperation(Queue<Long> queue, String operatorNum, IntegralLogStatusEnum status,
-			IntegralLogTypeEnum type) {
-		try {
-			boolean flag = true;
-			while (flag) {
-				Long id = queue.poll();
-				if (id == null) {
-					flag = false;
-					continue;
-				}
-				IntegralLog integralLog = integralLogRepository.findOne(id);
-				if (integralLog == null)
-					throw new Exception();
-				integralLog.setStatus(status);
-				integralLogRepository.saveAndFlush(integralLog);
-			}
-			return queue;
-		} catch (Exception e) {
-			LOGGER.info("someOperation:部分操作失败。错误信息：" + e.getMessage());
-			return null;
-		}
-	}
-
-	public IntegralLogInfo saveIntegralLog(IntegralLogInfo integralLogInfo) {
-		try {
-			IntegralLog integralLog = new IntegralLog();
-			UpicBeanUtils.copyProperties(integralLogInfo, integralLog);
-			integralLog = integralLogRepository.save(integralLog);
-			UpicBeanUtils.copyProperties(integralLog, integralLogInfo);
-		} catch (Exception e) {
-			LOGGER.info("saveIntegralLog:保存积分纪录失败。错误信息：" + e.getMessage());
-			return null;
-		}
-		return integralLogInfo;
-	}
-
-	public double watchIntegral(String studentNum) {
-		try {
-			// IntegralLogId integralLogId = new IntegralLogId();
-			// integralLogId.setStudentNum(studentNum);
-
-			return integralLogRepository.findByStudentNum(studentNum);
-		} catch (Exception e) {
-			LOGGER.info("watchIntegral:查看积分失败。错误信息：" + e.getMessage());
-			return 0;
-		}
-	}
-
-	public IntegralLogInfo getByIntegralLogId(IntegralLogIdInfo integralLogIdInfo) {
-		try {
-			IntegralLogId integralLogId = new IntegralLogId();
-			UpicBeanUtils.copyProperties(integralLogIdInfo, integralLogId);
-			IntegralLogInfo integralLogInfo = new IntegralLogInfo();
-			IntegralLog integralLog = integralLogRepository.findByIntegralLogId(integralLogId);
-			if (integralLog == null) {
-				return null;
-			} else {
-				UpicBeanUtils.copyProperties(integralLog, integralLogInfo);
-			}
-			return integralLogInfo;
-		} catch (Exception e) {
-			LOGGER.info("getByIntegralLogId:" + e.getMessage());
-			return null;
-		}
-	}
-
-	public IntegralLogInfo signUp(IntegralLogInfo integralLogInfo, int allNum) {
-		try {
-			Long num = redisComponent.increment(integralLogInfo.getIntegralLogId().getProjectNum());
-			if (num > allNum) {
-				throw new Exception("1001");
-			}
-			if(!redisComponent.putIfAbsent(integralLogInfo.getIntegralLogId().getProjectNum()+"hash",integralLogInfo.getIntegralLogId().getStudentNum(), num+"")) {
-				throw new Exception("1002");
-			}
-			integralLogInfo.setField1(num+"");
-			IntegralLog i=new IntegralLog();
-			UpicBeanUtils.copyProperties(integralLogInfo, i);
-			i = integralLogRepository.save(i);
-			UpicBeanUtils.copyProperties(i, integralLogInfo);
-			return integralLogInfo;
-		} catch (Exception e) {
-			redisComponent.decrement(integralLogInfo.getIntegralLogId().getProjectNum());
-			return null;
-		}
-
-	}
-
-	public Page<IntegralLogInfo> searchIntegralLog(IntegralLogCondition integralLogCondition, Pageable pageable) {
-		Page<IntegralLog> integralLogPage = null;
-		try {
-			integralLogPage = integralLogRepository.findAll(new IntegralLogSpec(integralLogCondition), pageable);
-			return QueryResultConverter.convert(integralLogPage, pageable,
-					new AbstractDomain2InfoConverter<IntegralLog, IntegralLogInfo>() {
-						protected void doConvert(IntegralLog domain, IntegralLogInfo info) throws Exception {
-							UpicBeanUtils.copyProperties(domain, info);
-						}
-					});
-		} catch (Exception e) {
-			LOGGER.info("searchIntegralLog:" + e.getMessage());
-			return null;
-		}
-	}
-
-	public Page<IntegralLogInfo> getUserListByProjectNum(String projectNum, Pageable pageable) {
-		Page<IntegralLog> integralLogPage = null;
-		try {
-			integralLogPage = integralLogRepository.getUserListByProjectNum(projectNum, pageable);
-			return QueryResultConverter.convert(integralLogPage, pageable,
-					new AbstractDomain2InfoConverter<IntegralLog, IntegralLogInfo>() {
-						protected void doConvert(IntegralLog domain, IntegralLogInfo info) throws Exception {
-							UpicBeanUtils.copyProperties(domain, info);
-						}
-					});
-		} catch (Exception e) {
-			LOGGER.info("getUserListByProjectNum:" + e.getMessage());
-			return null;
-		}
-	}
-
-	public int getSignUpNumberByProjectNum(String projectNum) {
-		try {
-			return integralLogRepository.getSignUpNumberByProjectNum(projectNum);
-		} catch (Exception e) {
-			LOGGER.info("getSignUpNumberByProjectNum:" + e.getMessage());
-			return 0;
-		}
-	}
-
-	public Page<IntegralLogInfo> getIntegralLogByMySelf(String studentNum, Pageable pageable) {
-		Page<IntegralLog> integralLogPage = null;
-		try {
-			integralLogPage = integralLogRepository.getIntegralLogByMySelf(studentNum, pageable);
-			return QueryResultConverter.convert(integralLogPage, pageable,
-					new AbstractDomain2InfoConverter<IntegralLog, IntegralLogInfo>() {
-						protected void doConvert(IntegralLog domain, IntegralLogInfo info) throws Exception {
-							UpicBeanUtils.copyProperties(domain, info);
-						}
-					});
-		} catch (Exception e) {
-			LOGGER.info("getIntegralLogByMySelf:" + e.getMessage());
-			return null;
-		}
-	}
-
-	public Page<IntegralLogInfo> getAllIntegralLogByStudentNum(String studentNum, Pageable pageable) {
-		Page<IntegralLog> integralLogPage = null;
-		try {
-			integralLogPage = integralLogRepository.getAllIntegralLogByStudentNum(studentNum, pageable);
-			return QueryResultConverter.convert(integralLogPage, pageable,
-					new AbstractDomain2InfoConverter<IntegralLog, IntegralLogInfo>() {
-						protected void doConvert(IntegralLog domain, IntegralLogInfo info) throws Exception {
-							UpicBeanUtils.copyProperties(domain, info);
-						}
-					});
-		} catch (Exception e) {
-			LOGGER.info("getAllIntegralLogByStudentNum:" + e.getMessage());
-			return null;
-		}
-	}
-
-	public Page<IntegralLogInfo> getIntegralLogDeclaring(String studentNum, Pageable pageable) {
-		Page<IntegralLog> integralLogPage = null;
-		try {
-			integralLogPage = integralLogRepository.getIntegralLogDeclaring(studentNum, pageable);
-			return QueryResultConverter.convert(integralLogPage, pageable,
-					new AbstractDomain2InfoConverter<IntegralLog, IntegralLogInfo>() {
-						protected void doConvert(IntegralLog domain, IntegralLogInfo info) throws Exception {
-							UpicBeanUtils.copyProperties(domain, info);
-						}
-					});
-		} catch (Exception e) {
-			LOGGER.info("getIntegralLogDeclaring:" + e.getMessage());
-			return null;
-		}
-	}
-
-	public Page<IntegralLogInfo> integralLogSearchBar(IntegralLogStatusEnum status, String keyword, Pageable pageable) {
-		Page<IntegralLog> integralLogPage = null;
-		try {
-			integralLogPage = integralLogRepository.integralLogSearchBar(status, keyword, pageable);
-			return QueryResultConverter.convert(integralLogPage, pageable,
-					new AbstractDomain2InfoConverter<IntegralLog, IntegralLogInfo>() {
-						@Override
-						protected void doConvert(IntegralLog domain, IntegralLogInfo info) throws Exception {
-							UpicBeanUtils.copyProperties(domain, info);
-						}
-					});
-		} catch (Exception e) {
-			LOGGER.info("integralLogSearchBar:" + e.getMessage());
-			return null;
-		}
-	}
-
-	public String updateIntegralLogStatus(List<IntegralLogIdInfo> integralLogIdInfos, IntegralLogStatusEnum status) {
-		try {
-			for (IntegralLogIdInfo integralLogIdInfo : integralLogIdInfos) {
-				IntegralLogId integralLogId = new IntegralLogId();
-				integralLogId.setStudentNum(integralLogIdInfo.getStudentNum());
-				integralLogId.setProjectNum(integralLogIdInfo.getProjectNum());
-				IntegralLog integralLog = integralLogRepository.findByIntegralLogId(integralLogId);
-				integralLog.setStatus(status);
-				integralLogRepository.saveAndFlush(integralLog);
-			}
-			return "SUCCESS";
-		} catch (Exception e) {
-			LOGGER.info("updateIntegralLogStatus:" + e.getMessage());
-			return null;
-		}
-	}
+	
 }
