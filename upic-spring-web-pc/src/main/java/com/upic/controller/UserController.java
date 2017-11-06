892eba06d11d9dcc0d6adfec47fc713411435e38
zhubuqing <1055793362@qq.com>
2017-11-06 18:19:43 +0800

    20171106005

--- upic-spring-web-pc/src/main/java/com/upic/controller/UserController.java
+++ upic-spring-web-pc/src/main/java/com/upic/controller/UserController.java
@@ -32,80 +32,81 @@ import com.upic.service.UserService;
 //@RestController
 @Controller
 public class UserController {
-	@Autowired
-	private WebRequestRedisService redisService;
+    @Autowired
+    private WebRequestRedisService redisService;
 
-	@Autowired
-	private ProviderSignInUtils providerSignInUtils;
+    @Autowired
+    private ProviderSignInUtils providerSignInUtils;
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@RequestMapping("/registUser")
-	public String createUser(WebRequest request, StudentInfo info) {
-		providerSignInUtils.doPostSignUp(info.getStuNum(), request);
-		UserInfo s = new UserInfo();
-		s.setUserNum(info.getStuNum());
-		s.setPassword(info.getPassword());
-		userService.addUser(s);
-		return "/login.html";
-	}
+    @RequestMapping("/registUser")
+    public String createUser(WebRequest request, StudentInfo info) {
+        providerSignInUtils.doPostSignUp(info.getStuNum(), request);
+        UserInfo s = new UserInfo();
+        s.setUserNum(info.getStuNum());
+        s.setPassword(info.getPassword());
+        userService.addUser(s);
+        return "/login.html";
+    }
 
-	@RequestMapping("/getUserInfo")
-	public StudentInfo readyRegist() {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		System.out.println(authentication);
-		if (authentication != null) {
-			System.out.println(authentication.getPrincipal());
-		}
-		SocialUser so = (SocialUser) authentication.getPrincipal();
-		StudentInfo s = new StudentInfo();
-		s.setStuNum(so.getUsername());
-		return s;
-	}
+    @RequestMapping("/getUserInfo")
+    public StudentInfo readyRegist() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(authentication);
+        if (authentication != null) {
+            System.out.println(authentication.getPrincipal());
+        }
+        SocialUser so = (SocialUser) authentication.getPrincipal();
+        StudentInfo s = new StudentInfo();
+        s.setStuNum(so.getUsername());
+        return s;
+    }
 
-	@RequestMapping("/cas")
-	public void createUsers(HttpServletRequest request, HttpServletResponse response, String sessionId) {
-		CASFilterRequestWrapper reqWrapper = new CASFilterRequestWrapper(request);
-		String userID = reqWrapper.getRemoteUser();
-		String payUrl = "/auth";// POST提交地址
-		String user = "user";// 如有多个，以此类推。
-		String pass = "pass";
+    @RequestMapping("/cas")
+    public void createUsers(HttpServletRequest request, HttpServletResponse response, String sessionId) {
+        CASFilterRequestWrapper reqWrapper = new CASFilterRequestWrapper(request);
+        String userID = reqWrapper.getRemoteUser();
+        String payUrl = "/auth";// POST提交地址
+        String user = "user";// 如有多个，以此类推。
+        String pass = "pass";
 
-		response.setContentType("text/html;charset=utf-8");
-		PrintWriter out;
-		try {
-			out = response.getWriter();
-			out.println("<form name='authSubmit' method='post'  action='" + payUrl + "' >");
-			out.println("<input type='hidden' name='" + user + "' value='" + userID + "'>"); // 如有多个，则写多个hidden即可
-			out.println("<input type='hidden' name='" + pass + "' value='" + userID + "'>");
-			out.println("</form>");
-			out.println("<script>");
-			out.println("  document.authSubmit.submit()");
-			out.println("</script>");
-		} catch (IOException e) {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out;
+        try {
+            out = response.getWriter();
+            out.println("<form name='authSubmit' method='post'  action='" + payUrl + "' >");
+            out.println("<input type='hidden' name='" + user + "' value='" + userID + "'>"); // 如有多个，则写多个hidden即可
+            out.println("<input type='hidden' name='" + pass + "' value='" + userID + "'>");
+            out.println("</form>");
+            out.println("<script>");
+            out.println("  document.authSubmit.submit()");
+            out.println("</script>");
+        } catch (IOException e) {
 
-		}
+        }
 
-	}
-	// @RequestMapping("/casgo")
-	// public String saveSession(WebRequest request,HttpServletRequest r,HttpSession
-	// session) {
-	// String sessionId = request.getSessionId();
-	// ProviderSignInAttempt attribute = (ProviderSignInAttempt)
-	// request.getAttribute(ProviderSignInAttempt.SESSION_ATTRIBUTE,
-	// RequestAttributes.SCOPE_SESSION);
-	// redisService.put(sessionId, attribute, 60*20);
-	// return "forward:/cas?sessionId="+sessionId;
-	// }
-	@RequestMapping("/logout")
-	public String logout(HttpSession session) {
+    }
+
+    // @RequestMapping("/casgo")
+    // public String saveSession(WebRequest request,HttpServletRequest r,HttpSession
+    // session) {
+    // String sessionId = request.getSessionId();
+    // ProviderSignInAttempt attribute = (ProviderSignInAttempt)
+    // request.getAttribute(ProviderSignInAttempt.SESSION_ATTRIBUTE,
+    // RequestAttributes.SCOPE_SESSION);
+    // redisService.put(sessionId, attribute, 60*20);
+    // return "forward:/cas?sessionId="+sessionId;
+    // }
+    @RequestMapping("/logout")
+    public String logout(HttpSession session) {
         Enumeration attributeNames = session.getAttributeNames();
-        while(attributeNames.hasMoreElements()){
+        while (attributeNames.hasMoreElements()) {
             String attrName = attributeNames.nextElement().toString();
             session.setAttribute(attrName, null);
             session.removeAttribute(attrName);
         }
-		return "/cas";
-	}
+        return "/cas";
+    }
 }
