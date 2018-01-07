5f5ebee85fcf997e848ad4e8a08f2eac14b5e40c
zhubuqing <1055793362@qq.com>
2018-01-07 20:52:11 +0800

    20180107003

--- upic-spring-web-pc/src/main/resources/resources/metroWeb/js/tjs/pic_manage.js
+++ upic-spring-web-pc/src/main/resources/resources/metroWeb/js/tjs/pic_manage.js
@@ -2,6 +2,10 @@
  * Created by zhubuqing on 2018/1/5.
  */
 var dataUrl = "/systemManager/getBanner";
+var getBannerByIdUrl = "/systemManager/getBannerById";
+var submitUrl = "/systemManager/uploadBanner";
+var updateBannerUrl = "/systemManager/updateBanner";
+var deleteBannerUrl = "/systemManager/deleteBanner";
 var pageSize = 0;
 var totalPages = -1;
 var pageNum = 0;
@@ -32,13 +36,102 @@ function addHtmls(datas, pageNum) {
         htmls += "</td>";
         htmls += "<td>" + data[i].field2 + "</td>";
         htmls += "<td class='center_td'>";
-        htmls += "<a href='#mymoda2' data-toggle='modal'>编辑</a><span class='space'>|</span><a href='#'>删除</a></td>";
+        htmls += "<a href='#mymoda2' data-toggle='modal'><div onclick=commonAjax('" + getBannerByIdUrl + "?id=" + data[i].id + "','','getBannerBrief','GET')>编辑</div></a><span class='space'>|</span><a href='#'><div onclick=commonAjax('" + deleteBannerUrl + "?id=" + data[i].id + "','','deleteBanner','GET')>删除</div></a></td>";
         htmls += "</tr>";
     }
     $("#data").html(htmls);
     page(datas, dataUrl, datas.size, datas.number);
 }
 
-function getProjectInfo(result, j) {
+function uploadPic() {
+    var formData = new FormData();
+    formData.append("url", $("#uploadPicUrl").val());
+    formData.append("field2", $("#uploadPicContent").val());
+    formData.append("file", $("#uploadPicFile")[0].files[0]);
+    $.ajax({
+        type: 'POST',
+        url: submitUrl,
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (result) {
+            if (result === "SUCCESS") {
+                alert("已提交");
+                getData(pageNum, dataUrl);
+            } else {
+                alert("提交失败");
+                getData(pageNum, dataUrl);
+            }
+        },
+        error: function (err) {
+            alert(err.msg);
+        }
+    });
+}
 
+function getBannerBrief(res) {
+    if (res != null) {
+        var bannerBrief = res;
+        var html = "";
+        html += "<div class='modal-header'>";
+        html += "<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>×</button>";
+        html += "<h4 id='mymodallabel11'>编辑</h4></div>";
+        html += "<div class='row-fluid'>";
+        html += "<div class='block-fluid'>";
+        html += "<div class='row-form clearfix'>";
+        html += "<div class='span3'>图片链接</div>";
+        html += "<div class='span6'><input id='" + bannerBrief.id + "updateBannerTheUrl' type='text' name='' value='" + bannerBrief.url + "'></div>";
+        html += "</div>";
+        html += "<div class='row-form clearfix'>";
+        html += "<div class='span3'>图片详情</div>";
+        html += "<div class='span6'><input id='" + bannerBrief.field2 + "updateBannerField2' type='text' name='' value='" + bannerBrief.field2 + "'></div>";
+        html += "</div>";
+        html += "<div class='row-form clearfix'>";
+        html += "<div class='span3'>选择图片</div>";
+        html += "<div class='span3'><input id='" + bannerBrief.id + "updateBannerPic' type='file'/></div>";
+        html += "</div></div><div class='dr'><span></span></div>";
+        html += "</div>";
+        html += "<div class='modal-footer'>";
+        html += "<button class='btn btn-primary' data-dismiss='modal' aria-hidden='true' onclick='updateBanner('" + bannerBrief.id + "')'>上传</button>";
+        html += "<button class='btn' data-dismiss='modal' aria-hidden='true'>取消</button>";
+        html += "</div>";
+        $("#mymoda2").html(html);
+    }
+}
+
+function updateBanner(bannerId) {
+    var formData = new FormData();
+    formData.append("id", bannerId);
+    formData.append("url", $("#" + bannerId + "updateBannerTheUrl").val());
+    formData.append("field2", $("#" + bannerId + "updateBannerField2").val());
+    formData.append("file", $("#" + bannerId + "updateBannerPic")[0].files[0]);
+    $.ajax({
+        type: 'POST',
+        url: updateBannerUrl,
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (result) {
+            if (result === "SUCCESS") {
+                alert("编辑成功");
+                getData(pageNum, dataUrl);
+            } else {
+                alert("编辑失败");
+                getData(pageNum, dataUrl);
+            }
+        },
+        error: function (err) {
+            alert(err.msg);
+        }
+    });
+}
+
+function deleteBanner(res) {
+    if (res == "SUCCESS") {
+        alert("删除成功");
+        getData(pageNum, dataUrl);
+    } else {
+        alert("删除失败");
+        getData(pageNum, dataUrl);
+    }
 }
